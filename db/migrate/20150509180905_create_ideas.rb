bde41c4909ca5d19dbbe317dcdcd38e98482cb5e
janineninonu <janinebagares@outlook.com>
2015-05-09 13:31:20 -0700

    first commit

---
+++ db/migrate/20150509180905_create_ideas.rb
@@ -0,0 +1,13 @@
+class CreateIdeas < ActiveRecord::Migration
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.string :description
+      t.string :text
+      t.string :picture
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
